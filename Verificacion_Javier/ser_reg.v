17593135c11b773dfaf2db133096ef569bbd918c
javierespinoza09 <javierespriv44@gmail.com>
2023-08-19 19:53:20 -0600

    Comando

--- Verificacion_Javier/ser_reg.v
+++ Verificacion_Javier/ser_reg.v
@@ -2,17 +2,17 @@ module ser_reg #(parameter Depth = 4) (
 	input rst, D_in, clk,
 	output [Depth-1:0] D_out)
 ;
-wire [Depth-1:0] d;
-wire [Depth-1:0] q;
+wire [Depth:0] d;
+//wire [Depth-1:0] q;
 genvar i;
 generate
 	for (i=0;i<Depth;i=i+1) 
 		begin
-			dff_arst u0 (.D(d[i]), .rst(rst), .clk(clk),.Q(q[i]));
+			dff_arst u0 (.D(d[i]), .rst(rst), .clk(clk),.Q(d[i+1]));
 		end
 endgenerate
 assign d[0] = D_in;
-assign d[Depth-1:1] = q[Depth-2:0];
-assign D_out = q;
+//assign d[Depth-1:1] = q[Depth-2:0];
+assign D_out = d[Depth:1];
 
 endmodule
