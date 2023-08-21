372ab762153a2eb1e7e586b73842fb262c8a30d2
Jordani Mejia <jordanymq99@gmail.com>
2023-08-21 15:06:18 -0600

    estandarizado el depth y bits

--- Veri_Jor/Mux_dff.sv
+++ Veri_Jor/Mux_dff.sv
@@ -6,21 +6,21 @@ module dff_arst #(parameter bits = 4) (
   output reg [bits-1:0] Q
 		);
   always@(posedge clk, posedge rst) begin
-	if(rst) Q <= 1'b0;
+    if(rst) Q <= 0;
 	else Q <= D;
 end
 endmodule
 
 
-module mux_dff #(parameter bits = 8, parameter depth = 4)(
+module mux_dff #(parameter depth = 8, parameter bits = 4)(
 input push,
-  input [depth-1:0] D_in,
+  input [bits-1:0] D_in,
 input reset,
-  output [depth-1:0] D_out [bits-1:0]
+  output [bits-1:0] D_out [depth-1:0]
 );
   genvar i;
   generate 
-    for (i=0;i<bits;i=i+1)begin
+    for (i=0;i<depth;i=i+1)begin
       if(i == 0) begin
         dff_arst dff_(.D(D_in),.rst(reset),.clk(push),.Q(D_out[i]));
       end else begin
@@ -33,15 +33,15 @@ input reset,
   
 endmodule
 
-module salida #(parameter depth = 8, parameter bits = 1)(
-  input [3:0] Data_in [7:0],
-input [3:0] pc,
-  output reg [3:0] Data_out);
+module salida #(parameter depth = 8, parameter bits = 4)(
+  input [bits-1:0] Data_in [depth-1:0],
+  input [bits-1:0] pc,
+  output reg [bits-1:0] Data_out);
   
-  always@(*)begin
+  always_comb begin
     case(pc)
-      0: Data_out<=0;
-      default: Data_out <= Data_in[pc-1];
+      0: Data_out =0;
+      default: Data_out = Data_in[pc-1];
     endcase 
   end 
   
@@ -53,13 +53,13 @@ endmodule
 
 
 // Code your design here
-module control #(parameter Depth = 4, parameter Bits = 8) (
+module control #(parameter bits = 4, parameter depth = 8) (
 input reset,
 input push,
 input pop,
 input clk,
 output push_out,
-  output reg [Depth-1:0] pc);
+  output reg [$clog2(depth):0] pc);
   parameter stay = 3'b000, out = 3'b001, add = 3'b010, actual = 3'b011, rst = 3'b100, empty = 3'b101, addone = 3'b111, zero = 3'b110 ;  
   
   always @(posedge clk) begin
@@ -67,7 +67,7 @@ output push_out,
       stay : pc = pc;
       empty : pc = 0;
       rst : pc = 0;
-      add : pc = (pc == Bits-1)? pc:pc+1;
+      add : pc = (pc == depth-1)? pc:pc+1;
       out : pc = (pc == 0)? 0:pc-1;
       actual : pc = pc;
       addone : pc = 1;
