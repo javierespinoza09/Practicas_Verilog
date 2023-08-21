f362ccb5f1a31dd0900c3ff19550b17921fb7dc2
Jordani Mejia <jordanymq99@gmail.com>
2023-08-21 14:49:04 -0600

    registros, mux y control

--- Veri_Jor/Mux_dff.sv
+++ Veri_Jor/Mux_dff.sv
@@ -1,19 +1,22 @@
-module dff_arst (
-		input D, rst, clk,
-		output reg Q
+// Code your design here
+// Code your design here
+module dff_arst #(parameter bits = 4) (
+  input [bits-1:0] D, 
+  input rst, clk,
+  output reg [bits-1:0] Q
 		);
-always@(posedge clk, negedge rst) begin
-	if(!rst) Q <= 1'b0;
+  always@(posedge clk, posedge rst) begin
+	if(rst) Q <= 1'b0;
 	else Q <= D;
 end
 endmodule
 
 
-module mux_dff #(parameter bits = 8)(
+module mux_dff #(parameter bits = 8, parameter depth = 4)(
 input push,
-input D_in,
+  input [depth-1:0] D_in,
 input reset,
-output [bits-1:0] D_out
+  output [depth-1:0] D_out [bits-1:0]
 );
   genvar i;
   generate 
@@ -31,12 +34,11 @@ output [bits-1:0] D_out
 endmodule
 
 module salida #(parameter depth = 8, parameter bits = 1)(
-input [depth-1:0] Data_in,
+  input [3:0] Data_in [7:0],
 input [3:0] pc,
-input clk,
-output reg Data_out);
+  output reg [3:0] Data_out);
   
-  always@(posedge clk)begin
+  always@(*)begin
     case(pc)
       0: Data_out<=0;
       default: Data_out <= Data_in[pc-1];
@@ -46,3 +48,33 @@ output reg Data_out);
   
   
 endmodule 
+
+
+
+
+// Code your design here
+module control #(parameter Depth = 4, parameter Bits = 8) (
+input reset,
+input push,
+input pop,
+input clk,
+output push_out,
+  output reg [Depth-1:0] pc);
+  parameter stay = 3'b000, out = 3'b001, add = 3'b010, actual = 3'b011, rst = 3'b100, empty = 3'b101, addone = 3'b111, zero = 3'b110 ;  
+  
+  always @(posedge clk) begin
+    case ({reset,push,pop})
+      stay : pc = pc;
+      empty : pc = 0;
+      rst : pc = 0;
+      add : pc = (pc == Bits-1)? pc:pc+1;
+      out : pc = (pc == 0)? 0:pc-1;
+      actual : pc = pc;
+      addone : pc = 1;
+      zero : pc = 0;
+      
+    endcase
+    
+  end
+  assign push_out = push;
+endmodule
--- Veri_Jor/Mux_dff_tb.sv
+++ Veri_Jor/Mux_dff_tb.sv
@@ -1,19 +1,27 @@
+// Code your testbench here
+// or browse Examples
 `timescale 1ns/10ps
 module muxdff_tb;
-reg reset_tb, push_tb,D_in_tb;
-reg [3:0] pc_tb;
-wire [7:0] D_out_tb;
-wire Data_out_tb;
+  reg reset_tb, push_tb, pop_tb,clk_tb;
+  reg [3:0] D_in_tb;
+wire [3:0] pc_tb;
+  wire [3:0] D_out_tb [7:0];
+  wire [3:0] Data_out_tb, push_out_tb;
   
+
+control DUT0 (
+  .reset(reset_tb), .push(push_tb), .pop(pop_tb), .clk(clk_tb),
+  .push_out(push_tb_out), .pc(pc_tb));
+
   
 mux_dff DUT1 (
-  .reset(reset_tb), .push(push_tb), .D_in(D_in_tb),
+  .reset(reset_tb), .push(push_tb_out), .D_in(D_in_tb),
   .D_out(D_out_tb)
 );
   
   
 salida DUT2(
-  .Data_in(D_out_tb), .pc(pc_tb), .clk(push_tb),
+  .Data_in(D_out_tb), .pc(pc_tb),
   .Data_out(Data_out_tb)
   );
   
@@ -27,31 +35,49 @@ end
 initial begin
 forever begin
 	#5
+  	clk_tb = ~clk_tb;
 	push_tb = ~push_tb;
 end
 end
   
 initial begin
+clk_tb = 1;
 reset_tb = 0;
 push_tb = 1;
 D_in_tb = 0;
-pc_tb = 0;
+  pop_tb = 0;
+
 
 #15
 reset_tb = 1;
-pc_tb = 1;
-
+#15
+reset_tb = 0;
 #15
 D_in_tb = 1;
 #15
-pc_tb = 6;
-#45
-D_in_tb = 0;
-#30
-reset_tb = 0;
+D_in_tb = 2;
 #15
-reset_tb = 1;
-#90
+D_in_tb = 3;
+#15
+D_in_tb = 4;
+
+#15
+D_in_tb = 5;
+
+#15
+D_in_tb = 6;
+pop_tb = 1;
+#15
+D_in_tb = 7;
+pop_tb = 0;
+#15
+D_in_tb = 8;
+#15
+D_in_tb = 9;
+#50
+
+
+#150
 
 
 $finish;
